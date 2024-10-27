416d3475b6f81ddd9b2cfd9f01866b4f173e939d
innno <banxiaqaq@gmail.com>
2024-10-27 11:38:01 +0800

    1

--- P52/52.cpp
+++ P52/52.cpp
@@ -10,7 +10,21 @@
 #define int long long
 using namespace std;
 
-void solve() {}
+void solve() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int x = 0;
+    while (n--) {
+      int a;
+      cin >> a;
+      x = x ^ a;
+    }
+    cout << x << endl;
+  }
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
