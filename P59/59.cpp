2b256b7e9e362b9fcc1a6e0b2301bd1021426280
innno <banxiaqaq@gmail.com>
2024-10-30 13:28:56 +0800

    1

--- P59/59.cpp
+++ P59/59.cpp
@@ -10,10 +10,30 @@
 #define int long long
 using namespace std;
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+  map<string, int> col;
+  vector<string> ve;
+  for (int i = 1; i <= n; i++) {
+    string s;
+    cin >> s;
+    if (col.count(s)) {
+      col[s]++;
+    } else {
+      ve.push_back(s);
+      col[s] = 1;
+    }
+  }
+  for (auto &i : ve) {
+    cout << i << " " << col[i] << "\n";
+  }
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-  solve();
+  int _;
+  cin >> _;
+  while (_--) solve();
   return 0;
 }
