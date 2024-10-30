52ee4fef69552c42c124d9d286674d577ed5fac6
innno <banxiaqaq@gmail.com>
2024-10-30 16:19:25 +0800

    更新 39.cpp

--- P39/39.cpp
+++ P39/39.cpp
@@ -10,10 +10,23 @@
 #define int long long
 using namespace std;
 
-void solve() {}
+const int N = 5e3 + 10, M = 5e5 + 10;
 
+void solve() {
+  int n;
+  cin >> n;
+  bitset<M> bs;
+  bs[0] = 1;
+  for (int i = 1; i <= n; i++) {
+    int a_i;
+    cin >> a_i;
+    bs |= (bs << a_i);
+  }
+  cout << bs.count();
+}
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-  solve();
+  int _ = 1;
+  while (_--) solve();
   return 0;
 }
