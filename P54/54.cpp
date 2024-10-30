05a91920d35492ff4e23059b5e16aa31b5de5c82
innno <banxiaqaq@gmail.com>
2024-10-30 13:56:25 +0800

    1

--- P54/54.cpp
+++ P54/54.cpp
@@ -9,36 +9,8 @@
 #define endl '\n'
 #define int long long
 using namespace std;
-const int N = 2e5 + 10;
 
-class Book {
- public:
-  int a, b, c;
-  // bool operator<(const Book &u) const {
-  // if (a == u.a && b == u.b) return c < u.c;
-  // if (a == u.a) return b < u.b;
-  // return a < u.a;
-  // }
-} p[N];
-
-bool cmp(const Book &u, const Book &v) {
-  if (u.a == v.a && u.b == v.b) return u.c > v.c;
-  if (u.a == v.a) return u.b > v.b;
-  return u.a > v.a;
-}
-
-void solve() {
-  int n;
-  cin >> n;
-  for (int i = 1; i <= n; i++) {
-    cin >> p[i].a >> p[i].b >> p[i].c;
-  }
-  sort(p + 1, p + 1 + n, cmp);
-  // reverse(p + 1, p + 1 + n);
-  for (int i = 1; i <= n; i++) {
-    cout << p[i].a << " " << p[i].b << " " << p[i].c << endl;
-  }
-}
+void solve() {}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
