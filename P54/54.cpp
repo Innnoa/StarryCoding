41d185afe31481e0c8b194ce64736920e9f5906c
innno <banxiaqaq@gmail.com>
2024-10-30 14:02:14 +0800

    更新 54.cpp

--- P54/54.cpp
+++ P54/54.cpp
@@ -10,7 +10,19 @@
 #define int long long
 using namespace std;
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+  set<int> a;
+  for (int i = 1; i <= n; i++) {
+    int a_i;
+    cin >> a_i;
+    a.insert(a_i);
+  }
+  for (auto& i : a) {
+    cout << i << " ";
+  }
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
