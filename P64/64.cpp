b4d329d2297944171e74c1a042dc466901c2bad8
innno <banxiaqaq@gmail.com>
2024-11-03 16:12:11 +0800

    更新 64.cpp

--- P64/64.cpp
+++ P64/64.cpp
@@ -9,8 +9,31 @@
 #define endl '\n'
 #define int long long
 using namespace std;
-
-void solve() {}
+const int N = 1e5 + 9;
+vector<int> v[N];
+bitset<N> vis;
+void find_v(int n) {
+  vis[n] = true;
+  for (auto& i : v[n]) {
+    if (vis[i]) {
+      continue;
+    }
+    find_v(i);
+  }
+}
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  for (int i = 1; i <= m; i++) {
+    int k, j;
+    cin >> k >> j;
+    if (k != j) v[k].push_back(j);
+  }
+  find_v(1);
+  for (int i = 1; i <= n; i++) {
+    if (vis[i]) cout << i << " ";
+  }
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
