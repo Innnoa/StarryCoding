0fd6af2aca36fd48556b500cee7ffe59ca1d51e5
innno <banxiaqaq@gmail.com>
2024-11-05 14:30:55 +0800

    更新 67.cpp

--- P67/67.cpp
+++ P67/67.cpp
@@ -13,7 +13,9 @@ using namespace std;
 void solve() {}
 
 signed main() {
-  ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-  solve();
+  ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  int _ = 1;
+  while (_--) solve();
+
   return 0;
 }
