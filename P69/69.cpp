8ccee4c858429f6850be4a9b03f6759e5546cf10
innno <banxiaqaq@gmail.com>
2024-11-05 15:48:09 +0800

    1

---
+++ P69/69.cpp
@@ -0,0 +1,19 @@
+// Problem: ${json.name}
+// Contest: ${json.group}
+// URL: ${json.url}
+// Memory Limit: ${json.memoryLimit} MB
+// Time Limit: ${json.timeLimit} ms
+// More thought is needed
+
+#include <bits/stdc++.h>
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void solve() {}
+
+signed main() {
+  ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+  solve();
+  return 0;
+}
