6cac7406bc40f2447bf939871f744d327b420f7f
innno <banxiaqaq@gmail.com>
2024-11-06 15:27:05 +0800

    更新 17.cpp

--- P17/17.cpp
+++ P17/17.cpp
@@ -9,8 +9,26 @@
 #define endl '\n'
 #define int long long
 using namespace std;
-
-void solve() {}
+const int N = 1e5 + 9;
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> res;
+  for (int i = 2; i <= n / i; i++) {
+    if (n % i) {
+      continue;
+    }
+    res.push_back(i);
+    while (n % i == 0) {
+      n /= i;
+    }
+  }
+  if (n > 1) res.push_back(n);
+  sort(res.begin(), res.end());
+  for (auto& i : res) {
+    cout << i << " ";
+  }
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
