04dad9fcfb07fe3ca10e59a8bdb3d6ec14fa3cd0
innno <banxiaqaq@gmail.com>
2024-11-06 15:11:11 +0800

    更新 16.cpp

--- P16/16.cpp
+++ P16/16.cpp
@@ -9,8 +9,22 @@
 #define endl '\n'
 #define int long long
 using namespace std;
-
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> res;
+  for (int i = 1; i <= n / i; i++) {
+    if (n % i == 0) {
+      res.push_back(i);
+      res.push_back(n / i);
+    }
+  }
+  sort(res.begin(), res.end());
+  res.erase(unique(res.begin(), res.end()), res.end());
+  for (auto& i : res) {
+    cout << i << " ";
+  }
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
