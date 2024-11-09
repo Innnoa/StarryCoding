0e970e75616d88bb9b44ff2f3a9ac16c4060f173
innno <banxiaqaq@gmail.com>
2024-11-09 11:59:53 +0800

    1

--- P44/44.cpp
+++ P44/44.cpp
@@ -9,11 +9,36 @@
 #define endl '\n'
 #define int long long
 using namespace std;
+const int N = 998244353;
 
-void solve() {}
+int fastabs(int a, int b) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res = res * a % N;
+    a = a * a % N;
+    b >>= 1;
+  }
+  return res;
+}
+
+int MOX(int a, int b, int c, int x) {
+  return ((a * x % N + b) % N * fastabs(c * x % N, N - 2) % N) % N;
+}
+
+void solve() {
+  int a, b, c, q;
+  cin >> a >> b >> c >> q;
+  for (int i = 1; i <= q; i++) {
+    int x;
+    cin >> x;
+    cout << MOX(a, b, c, x) << endl;
+  }
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-  solve();
+  int _;
+  cin >> _;
+  while (_--) solve();
   return 0;
 }
--- P66/66.cpp
+++ P66/66.cpp
@@ -10,10 +10,26 @@
 #define int long long
 using namespace std;
 
-void solve() {}
+int fastabs(int a, int b, int c) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res = res * a % c;
+    a = a * a % c;
+    b >>= 1;
+  }
+  return res;
+}
+
+void solve() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << fastabs(a, b, c) << endl;
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-  solve();
+  int _;
+  cin >> _;
+  while (_--) solve();
   return 0;
 }
