e39c0eec176fd249fe479fec4ff213f6f8f4b445
innno <banxiaqaq@gmail.com>
2024-11-14 15:11:37 +0800

    1

--- P45/45.cpp
+++ P45/45.cpp
@@ -9,8 +9,21 @@
 #define endl '\n'
 #define int long long
 using namespace std;
-
-void solve() {}
+const int N = 1e3 + 9;
+const int M = 1e9 + 7;
+int a[N][N];
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  a[0][0] = 1;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      a[i][j] = (a[i - 1][j - 1] % M + a[i - 1][j] % M) % M;
+      cout << a[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
