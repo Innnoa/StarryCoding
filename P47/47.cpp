8ccea73fb0c4e683453f2a1c191dd6c6abd04ae0
innno <banxiaqaq@gmail.com>
2024-11-16 10:53:11 +0800

    1

--- P47/47.cpp
+++ P47/47.cpp
@@ -9,8 +9,27 @@
 #define endl '\n'
 #define int long long
 using namespace std;
-
-void solve() {}
+const int N = 5e2 + 5;
+const int M = 1e5 + 5;
+int w[N], v[N];
+int dp[2][M];
+void solve() {
+  int m, n;
+  cin >> m >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> w[i] >> v[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= m; j++) {
+      int y = i & 1;
+      if (j >= v[i])
+        dp[y][j] = max(dp[y ^ 1][j], dp[y][j - v[i]] + w[i]);
+      else
+        dp[y][j] = dp[y ^ 1][j];
+    }
+  }
+  cout << dp[n & 1][m];
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
