317aceb6cca591b6d46827da108506c820a1feb8
innno <banxiaqaq@gmail.com>
2024-11-17 15:48:37 +0800

    更新 75.cpp

--- P75/75.cpp
+++ P75/75.cpp
@@ -9,8 +9,26 @@
 #define endl '\n'
 #define int long long
 using namespace std;
-
-void solve() {}
+const int N = 1e2 + 5;
+const int M = 1e4 + 9;
+int dp[M];
+void solve() {
+  int m, n;
+  cin >> m >> n;
+  for (int i = 1; i <= n; i++) {
+    int s, w, v;
+    cin >> s >> w >> v;
+    while (s--) {
+      for (int j = m; j >= 0; j--) {
+        if (j >= v)
+          dp[j] = max(dp[j], dp[j - v] + w);
+        else
+          dp[j] = dp[j];
+      }
+    }
+  }
+  cout << dp[m];
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
