237fcc5c32a6d4a1c95b922e12d865930e605c9c
innno <banxiaqaq@gmail.com>
2024-11-17 15:57:21 +0800

    更新 74.cpp

--- P74/74.cpp
+++ P74/74.cpp
@@ -11,19 +11,20 @@
 using namespace std;
 int T, M;
 int t[105], v[105];
-int dp[1009];
+int dp[2][1009];
 void solve() {
-  for (int i = 0; i <= T; i++) dp[i] = 0;
+  for (int i = 0; i <= T; i++) dp[0][i] = 0;  //清空,多次询问
   for (int i = 1; i <= M; i++) cin >> t[i] >> v[i];
   for (int i = 1; i <= M; i++) {
-    for (int j = T; j >= 0; j--) {
+    int y = i & 1;
+    for (int j = 0; j <= T; j++) {
       if (j >= t[i])
-        dp[j] = max(dp[j], dp[j - t[i]] + v[i]);
+        dp[y][j] = max(dp[y ^ 1][j], dp[y ^ 1][j - t[i]] + v[i]);
       else
-        dp[j] = dp[j];
+        dp[y][j] = dp[y ^ 1][j];
     }
   }
-  cout << dp[T] << endl;
+  cout << dp[M & 1][T] << endl;
 }
 
 signed main() {
