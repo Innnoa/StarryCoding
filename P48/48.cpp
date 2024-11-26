dcaafcf5f1b41d2b85a2f8d92261fece869b84d7
innno <banxiaqaq@gmail.com>
2024-11-26 15:23:19 +0800

    1

--- P48/48.cpp
+++ P48/48.cpp
@@ -9,8 +9,22 @@
 #define endl '\n'
 #define int long long
 using namespace std;
-
-void solve() {}
+const int N = 2e5 + 9;
+int a[N];
+int stk[N], top;
+void solve() {
+  int n;
+  cin >> n;
+  int ans = 0;
+  for (int i = 1; i <= n; i++) cin >> a[i];
+  for (int i = 1; i <= n; i++) {
+    int pos = upper_bound(stk + 1, stk + 1 + top, a[i]) - stk;
+    if (pos == top + 1) top++;
+    stk[pos] = a[i];
+    ans = max(ans, top);
+  }
+  cout << ans;
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
--- P77/77.cpp
+++ P77/77.cpp
@@ -9,8 +9,25 @@
 #define endl '\n'
 #define int long long
 using namespace std;
+const int N = 2e5 + 9;
+int a[N], dp[N];
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+  int max_n = 1;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    dp[i] = 1;
+    for (int j = i - 1; j > 0; j--) {
+      if (a[i] >= a[j]) dp[i] = max(dp[j] + 1, dp[i]);
+      max_n = max(max_n, dp[i]);
+    }
+  }
+  cout << max_n;
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
