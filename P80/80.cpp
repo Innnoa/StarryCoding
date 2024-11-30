0709fbf75bef3df8ed467e19705ffea2135b6b06
innno <banxiaqaq@gmail.com>
2024-11-30 11:00:39 +0800

    更新 80.cpp

--- P80/80.cpp
+++ P80/80.cpp
@@ -9,8 +9,23 @@
 #define endl '\n'
 #define int long long
 using namespace std;
-
-void solve() {}
+const int N = 1e6 + 7;
+int a[105];
+int dp[105][105];
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) cin >> a[i];
+  dp[0][0] = 1;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= m; j++) {
+      for (int k = 0; k <= a[i] && k <= j; k++) {
+        dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % N;
+      }
+    }
+  }
+  cout << dp[n][m];
+}
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
